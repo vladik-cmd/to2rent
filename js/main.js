7c8eb854aca15939eb79f19ebb60376fa363760c
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-04-19 22:16:22 +0300

    адаптив

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function(){
 $('.slider').slick({
-nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="prev"></button>',
-prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
 fade: true,
 responsive: [
     {
       breakpoint: 441,
       settings: {
-        arrows: false
+        arrows : false,
       }
     }
     ]
